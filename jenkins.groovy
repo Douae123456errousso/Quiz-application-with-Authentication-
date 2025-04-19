788dfb1b830165c5fac3bce3a304a095096cf870
douae <douae.errousso@esi.ac.ma>
2025-04-19 15:58:08 +0100

    modification de fichier jenkins

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,8 +2,13 @@ pipeline {
     agent any
 
     tools {
-        maven 'Maven 3.8.8'   
-        jdk 'JDK 17'            
+        maven 'Maven 3.8.8'
+        jdk 'JDK 17'
+    }
+
+    environment {
+        DEPLOY_DIR = 'C:\\deploy\\quiz-app'
+        JAR_NAME = 'quiz-app.jar' // change si ton jar a un autre nom
     }
 
     stages {
@@ -13,13 +18,13 @@ pipeline {
             }
         }
 
-        stage('Compile') {
+        stage('Build') {
             steps {
-                bat 'mvn compile'
+                bat 'mvn clean compile'
             }
         }
 
-        stage('Test') {
+        stage('Tests') {
             steps {
                 bat 'mvn test'
             }
@@ -30,15 +35,43 @@ pipeline {
                 bat 'mvn jacoco:report'
             }
         }
+
+        stage('Package') {
+            steps {
+                bat 'mvn package -DskipTests'
+            }
+        }
+
+        stage('Deploy Locally') {
+            steps {
+                script {
+                    // Créer le dossier si besoin
+                    bat "if not exist %DEPLOY_DIR% mkdir %DEPLOY_DIR%"
+
+                    // Copier le JAR (le plus récent dans target)
+                    bat 'for /f %%f in (\'dir /b /od target\\*.jar\') do set LAST_JAR=%%f'
+                    bat 'copy target\\%LAST_JAR% %DEPLOY_DIR%\\%JAR_NAME% /Y'
+
+                    // Tuer un ancien process Java si l'app est déjà lancée (optionnel)
+                    bat 'for /f "tokens=2" %%i in (\'tasklist ^| findstr java.exe\') do taskkill /PID %%i /F'
+
+                    // Lancer l'application
+                    bat "start java -jar %DEPLOY_DIR%\\%JAR_NAME%"
+                }
+            }
+        }
     }
 
     post {
         always {
             junit 'target\\surefire-reports\\*.xml'
-            archiveArtifacts artifacts: 'target\\site\\jacoco\\**', fingerprint: true
+            archiveArtifacts artifacts: 'target\\site\\jacoco\\', fingerprint: true
         }
         failure {
-            echo 'Le build a échoué.'
+            echo '❌ Le pipeline a échoué.'
+        }
+        success {
+            echo '✅ Déploiement local terminé ! L’application est lancée.'
         }
     }
 }
